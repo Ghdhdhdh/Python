44850a4da29e801844c087c56c3c8f649d897923
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2025-06-18 09:28:04 +1000

    Update numbers_and_math.py

--- 1.Numbers_Operators_Comments/numbers_and_math.py
+++ 1.Numbers_Operators_Comments/numbers_and_math.py
@@ -1,20 +1,16 @@
 def main():
-    # ints: 4, 57, and -10
-    # floating point: 6.1, 1.3333, 0.0
-    print("Then number 10 is of type...")
-    print(type(10))
-    print("The number 11.23 is of type...")
-    print(type(11.23))
-    print()
-    print("1 + 1 =", 1+1)
-    print("5 - 3 =", 5 - 3)
-    # single / will return a float
-    print("18 / 2 =", 18 / 2)
-    # double // will return a int (floored division)
-    print("18 // 2 = ", 18 // 2)
-    print("15 % 2 =", 15 % 2)
-    # ** exponent
-    print("2 ** 3 =", 2 ** 3)
+    things_to_print = {
+        "Then number 10 is of type..." : type(10),
+        "The number 11.23 is of type..." : type(11.23)
+        "1 + 1 = ": 1+1,
+        "5 - 3 = ": 5-3,
+        "18 / 2 = ": 18 / 2,
+        "18 // 2 = ": 18//2,
+        "15 % 2 = ":15 % 2,
+        "2 ** 3 = ": 2**3}
+
+    for k,v in things_to_print.items():
+        print(f"{k}{v}")
 
 
 if __name__ == "__main__":
